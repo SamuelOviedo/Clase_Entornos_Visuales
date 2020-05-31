8c1428102311c6503e24ad9fd645a465d92a8014
SamuelOviedo <65907486+SamuelOviedo@users.noreply.github.com>
2020-05-30 21:30:18 -0600

    Add files via upload

---
+++ I Parcial_Programas clase/Suma de dos valores/Suma de dos valores/Form1.vb
@@ -0,0 +1,24 @@
+﻿Public Class Form1
+
+
+    Private Sub btnSalir_Click(sender As Object, e As EventArgs) Handles btnSalir.Click
+        End
+
+    End Sub
+
+    Private Sub btnCalcular_Click(sender As Object, e As EventArgs) Handles btnCalcular.Click
+        Dim valor1 As Integer
+        Dim valor2 As Integer
+        Dim suma As Integer
+
+        valor1 = Val(txtValor1.Text)
+        valor2 = Val(txtValor2.Text)
+
+        'proceso
+        suma = valor1 + valor2
+
+        'salida
+        txtResultado.Text = suma
+
+    End Sub
+End Class
