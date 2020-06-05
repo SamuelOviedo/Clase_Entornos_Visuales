cb9eeb22031fc38af648fb8d6874d72af4938e00
Samuel Oviedo <isaaccoviedo@gmail.com>
2020-06-05 17:12:23 -0600

    calculadora

---
+++ I Parcial/Programas tarea/Programas guía/Calculadora básica/Calculadora básica/Form1.Designer.vb
@@ -0,0 +1,360 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtDatos = New System.Windows.Forms.TextBox()
+        Me.btn7 = New System.Windows.Forms.Button()
+        Me.btn4 = New System.Windows.Forms.Button()
+        Me.btn1 = New System.Windows.Forms.Button()
+        Me.btnCero = New System.Windows.Forms.Button()
+        Me.btnMasMenos = New System.Windows.Forms.Button()
+        Me.btn2 = New System.Windows.Forms.Button()
+        Me.btn5 = New System.Windows.Forms.Button()
+        Me.btn8 = New System.Windows.Forms.Button()
+        Me.btnPunto = New System.Windows.Forms.Button()
+        Me.btn3 = New System.Windows.Forms.Button()
+        Me.btn6 = New System.Windows.Forms.Button()
+        Me.btn9 = New System.Windows.Forms.Button()
+        Me.btnMas = New System.Windows.Forms.Button()
+        Me.btnMenos = New System.Windows.Forms.Button()
+        Me.btnPor = New System.Windows.Forms.Button()
+        Me.btnEntre = New System.Windows.Forms.Button()
+        Me.btnLimpiar = New System.Windows.Forms.Button()
+        Me.btnIgual = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lbHistorial = New System.Windows.Forms.ListBox()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'txtDatos
+        '
+        Me.txtDatos.BackColor = System.Drawing.Color.Silver
+        Me.txtDatos.Font = New System.Drawing.Font("Rockwell", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtDatos.Location = New System.Drawing.Point(17, 26)
+        Me.txtDatos.Name = "txtDatos"
+        Me.txtDatos.Size = New System.Drawing.Size(559, 39)
+        Me.txtDatos.TabIndex = 0
+        Me.txtDatos.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'btn7
+        '
+        Me.btn7.BackColor = System.Drawing.Color.DarkGray
+        Me.btn7.Font = New System.Drawing.Font("Sigmar One", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn7.Location = New System.Drawing.Point(17, 82)
+        Me.btn7.Name = "btn7"
+        Me.btn7.Size = New System.Drawing.Size(79, 73)
+        Me.btn7.TabIndex = 1
+        Me.btn7.Text = "7"
+        Me.btn7.UseVisualStyleBackColor = False
+        '
+        'btn4
+        '
+        Me.btn4.BackColor = System.Drawing.Color.DarkGray
+        Me.btn4.Font = New System.Drawing.Font("Sigmar One", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn4.Location = New System.Drawing.Point(17, 164)
+        Me.btn4.Name = "btn4"
+        Me.btn4.Size = New System.Drawing.Size(79, 73)
+        Me.btn4.TabIndex = 2
+        Me.btn4.Text = "4"
+        Me.btn4.UseVisualStyleBackColor = False
+        '
+        'btn1
+        '
+        Me.btn1.BackColor = System.Drawing.Color.DarkGray
+        Me.btn1.Font = New System.Drawing.Font("Sigmar One", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn1.Location = New System.Drawing.Point(17, 254)
+        Me.btn1.Name = "btn1"
+        Me.btn1.Size = New System.Drawing.Size(79, 73)
+        Me.btn1.TabIndex = 3
+        Me.btn1.Text = "1"
+        Me.btn1.UseVisualStyleBackColor = False
+        '
+        'btnCero
+        '
+        Me.btnCero.BackColor = System.Drawing.Color.DarkGray
+        Me.btnCero.Font = New System.Drawing.Font("Sigmar One", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCero.Location = New System.Drawing.Point(17, 337)
+        Me.btnCero.Name = "btnCero"
+        Me.btnCero.Size = New System.Drawing.Size(79, 73)
+        Me.btnCero.TabIndex = 4
+        Me.btnCero.Text = "0"
+        Me.btnCero.UseVisualStyleBackColor = False
+        '
+        'btnMasMenos
+        '
+        Me.btnMasMenos.BackColor = System.Drawing.Color.DarkGray
+        Me.btnMasMenos.Font = New System.Drawing.Font("Sigmar One", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnMasMenos.Location = New System.Drawing.Point(125, 339)
+        Me.btnMasMenos.Name = "btnMasMenos"
+        Me.btnMasMenos.Size = New System.Drawing.Size(79, 73)
+        Me.btnMasMenos.TabIndex = 8
+        Me.btnMasMenos.Text = "+/-"
+        Me.btnMasMenos.UseVisualStyleBackColor = False
+        '
+        'btn2
+        '
+        Me.btn2.BackColor = System.Drawing.Color.DarkGray
+        Me.btn2.Font = New System.Drawing.Font("Sigmar One", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn2.Location = New System.Drawing.Point(125, 256)
+        Me.btn2.Name = "btn2"
+        Me.btn2.Size = New System.Drawing.Size(79, 73)
+        Me.btn2.TabIndex = 7
+        Me.btn2.Text = "2"
+        Me.btn2.UseVisualStyleBackColor = False
+        '
+        'btn5
+        '
+        Me.btn5.BackColor = System.Drawing.Color.DarkGray
+        Me.btn5.Font = New System.Drawing.Font("Sigmar One", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn5.Location = New System.Drawing.Point(125, 166)
+        Me.btn5.Name = "btn5"
+        Me.btn5.Size = New System.Drawing.Size(79, 73)
+        Me.btn5.TabIndex = 6
+        Me.btn5.Text = "5"
+        Me.btn5.UseVisualStyleBackColor = False
+        '
+        'btn8
+        '
+        Me.btn8.BackColor = System.Drawing.Color.DarkGray
+        Me.btn8.Font = New System.Drawing.Font("Sigmar One", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn8.Location = New System.Drawing.Point(125, 84)
+        Me.btn8.Name = "btn8"
+        Me.btn8.Size = New System.Drawing.Size(79, 73)
+        Me.btn8.TabIndex = 5
+        Me.btn8.Text = "8"
+        Me.btn8.UseVisualStyleBackColor = False
+        '
+        'btnPunto
+        '
+        Me.btnPunto.BackColor = System.Drawing.Color.DarkGray
+        Me.btnPunto.Font = New System.Drawing.Font("Sigmar One", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnPunto.Location = New System.Drawing.Point(233, 337)
+        Me.btnPunto.Name = "btnPunto"
+        Me.btnPunto.Size = New System.Drawing.Size(79, 73)
+        Me.btnPunto.TabIndex = 12
+        Me.btnPunto.Text = "."
+        Me.btnPunto.UseVisualStyleBackColor = False
+        '
+        'btn3
+        '
+        Me.btn3.BackColor = System.Drawing.Color.DarkGray
+        Me.btn3.Font = New System.Drawing.Font("Sigmar One", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn3.Location = New System.Drawing.Point(233, 254)
+        Me.btn3.Name = "btn3"
+        Me.btn3.Size = New System.Drawing.Size(79, 73)
+        Me.btn3.TabIndex = 11
+        Me.btn3.Text = "3"
+        Me.btn3.UseVisualStyleBackColor = False
+        '
+        'btn6
+        '
+        Me.btn6.BackColor = System.Drawing.Color.DarkGray
+        Me.btn6.Font = New System.Drawing.Font("Sigmar One", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn6.Location = New System.Drawing.Point(233, 164)
+        Me.btn6.Name = "btn6"
+        Me.btn6.Size = New System.Drawing.Size(79, 73)
+        Me.btn6.TabIndex = 10
+        Me.btn6.Text = "6"
+        Me.btn6.UseVisualStyleBackColor = False
+        '
+        'btn9
+        '
+        Me.btn9.BackColor = System.Drawing.Color.DarkGray
+        Me.btn9.Font = New System.Drawing.Font("Sigmar One", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn9.Location = New System.Drawing.Point(233, 82)
+        Me.btn9.Name = "btn9"
+        Me.btn9.Size = New System.Drawing.Size(79, 73)
+        Me.btn9.TabIndex = 9
+        Me.btn9.Text = "9"
+        Me.btn9.UseVisualStyleBackColor = False
+        '
+        'btnMas
+        '
+        Me.btnMas.BackColor = System.Drawing.Color.DarkGray
+        Me.btnMas.Font = New System.Drawing.Font("Sigmar One", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnMas.Location = New System.Drawing.Point(339, 337)
+        Me.btnMas.Name = "btnMas"
+        Me.btnMas.Size = New System.Drawing.Size(79, 73)
+        Me.btnMas.TabIndex = 16
+        Me.btnMas.Text = "+"
+        Me.btnMas.UseVisualStyleBackColor = False
+        '
+        'btnMenos
+        '
+        Me.btnMenos.BackColor = System.Drawing.Color.DarkGray
+        Me.btnMenos.Font = New System.Drawing.Font("Sigmar One", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnMenos.Location = New System.Drawing.Point(339, 254)
+        Me.btnMenos.Name = "btnMenos"
+        Me.btnMenos.Size = New System.Drawing.Size(79, 73)
+        Me.btnMenos.TabIndex = 15
+        Me.btnMenos.Text = "-"
+        Me.btnMenos.UseVisualStyleBackColor = False
+        '
+        'btnPor
+        '
+        Me.btnPor.BackColor = System.Drawing.Color.DarkGray
+        Me.btnPor.Font = New System.Drawing.Font("Sigmar One", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnPor.Location = New System.Drawing.Point(339, 164)
+        Me.btnPor.Name = "btnPor"
+        Me.btnPor.Size = New System.Drawing.Size(79, 73)
+        Me.btnPor.TabIndex = 14
+        Me.btnPor.Text = "x"
+        Me.btnPor.UseVisualStyleBackColor = False
+        '
+        'btnEntre
+        '
+        Me.btnEntre.BackColor = System.Drawing.Color.DarkGray
+        Me.btnEntre.Font = New System.Drawing.Font("Sigmar One", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnEntre.Location = New System.Drawing.Point(339, 82)
+        Me.btnEntre.Name = "btnEntre"
+        Me.btnEntre.Size = New System.Drawing.Size(79, 73)
+        Me.btnEntre.TabIndex = 13
+        Me.btnEntre.Text = "/"
+        Me.btnEntre.UseVisualStyleBackColor = False
+        '
+        'btnLimpiar
+        '
+        Me.btnLimpiar.BackColor = System.Drawing.Color.DarkGray
+        Me.btnLimpiar.Font = New System.Drawing.Font("Russo One", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnLimpiar.Location = New System.Drawing.Point(450, 84)
+        Me.btnLimpiar.Name = "btnLimpiar"
+        Me.btnLimpiar.Size = New System.Drawing.Size(126, 152)
+        Me.btnLimpiar.TabIndex = 17
+        Me.btnLimpiar.Text = "Limpiar"
+        Me.btnLimpiar.UseVisualStyleBackColor = False
+        '
+        'btnIgual
+        '
+        Me.btnIgual.BackColor = System.Drawing.Color.DarkGray
+        Me.btnIgual.Font = New System.Drawing.Font("Russo One", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnIgual.Location = New System.Drawing.Point(450, 254)
+        Me.btnIgual.Name = "btnIgual"
+        Me.btnIgual.Size = New System.Drawing.Size(126, 156)
+        Me.btnIgual.TabIndex = 18
+        Me.btnIgual.Text = "="
+        Me.btnIgual.UseVisualStyleBackColor = False
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.BackColor = System.Drawing.Color.DarkSlateGray
+        Me.GroupBox1.Controls.Add(Me.GroupBox2)
+        Me.GroupBox1.Controls.Add(Me.btnIgual)
+        Me.GroupBox1.Controls.Add(Me.btnLimpiar)
+        Me.GroupBox1.Controls.Add(Me.btnMas)
+        Me.GroupBox1.Controls.Add(Me.btnMenos)
+        Me.GroupBox1.Controls.Add(Me.btnPor)
+        Me.GroupBox1.Controls.Add(Me.btnEntre)
+        Me.GroupBox1.Controls.Add(Me.btnPunto)
+        Me.GroupBox1.Controls.Add(Me.btn3)
+        Me.GroupBox1.Controls.Add(Me.btn6)
+        Me.GroupBox1.Controls.Add(Me.btn9)
+        Me.GroupBox1.Controls.Add(Me.btnMasMenos)
+        Me.GroupBox1.Controls.Add(Me.btn2)
+        Me.GroupBox1.Controls.Add(Me.btn5)
+        Me.GroupBox1.Controls.Add(Me.btn8)
+        Me.GroupBox1.Controls.Add(Me.btnCero)
+        Me.GroupBox1.Controls.Add(Me.btn1)
+        Me.GroupBox1.Controls.Add(Me.btn4)
+        Me.GroupBox1.Controls.Add(Me.btn7)
+        Me.GroupBox1.Controls.Add(Me.txtDatos)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(818, 446)
+        Me.GroupBox1.TabIndex = 19
+        Me.GroupBox1.TabStop = False
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.lbHistorial)
+        Me.GroupBox2.Controls.Add(Me.Label1)
+        Me.GroupBox2.Location = New System.Drawing.Point(607, 21)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(181, 388)
+        Me.GroupBox2.TabIndex = 19
+        Me.GroupBox2.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.DarkGray
+        Me.Label1.Font = New System.Drawing.Font("Rockwell", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(17, 18)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(122, 31)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Historial"
+        '
+        'lbHistorial
+        '
+        Me.lbHistorial.BackColor = System.Drawing.Color.DarkGray
+        Me.lbHistorial.FormattingEnabled = True
+        Me.lbHistorial.ItemHeight = 16
+        Me.lbHistorial.Location = New System.Drawing.Point(14, 64)
+        Me.lbHistorial.Name = "lbHistorial"
+        Me.lbHistorial.Size = New System.Drawing.Size(156, 308)
+        Me.lbHistorial.TabIndex = 1
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Gray
+        Me.ClientSize = New System.Drawing.Size(846, 472)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "Calculadora"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents txtDatos As TextBox
+    Friend WithEvents btn7 As Button
+    Friend WithEvents btn4 As Button
+    Friend WithEvents btn1 As Button
+    Friend WithEvents btnCero As Button
+    Friend WithEvents btnMasMenos As Button
+    Friend WithEvents btn2 As Button
+    Friend WithEvents btn5 As Button
+    Friend WithEvents btn8 As Button
+    Friend WithEvents btnPunto As Button
+    Friend WithEvents btn3 As Button
+    Friend WithEvents btn6 As Button
+    Friend WithEvents btn9 As Button
+    Friend WithEvents btnMas As Button
+    Friend WithEvents btnMenos As Button
+    Friend WithEvents btnPor As Button
+    Friend WithEvents btnEntre As Button
+    Friend WithEvents btnLimpiar As Button
+    Friend WithEvents btnIgual As Button
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents lbHistorial As ListBox
+    Friend WithEvents Label1 As Label
+End Class
---
+++ I Parcial/Programas tarea/Programas guía/Calculadora básica/Calculadora básica/Form1.vb
@@ -0,0 +1,137 @@
+﻿Public Class Form1
+
+    Dim operacion As String
+    Dim numeroResultado As Nullable(Of Double) = Nothing
+    Dim segundoValor As Nullable(Of Double) = Nothing
+    Dim sePresionaOperador As Boolean
+
+    Private Sub btnIgual_Click(sender As Object, e As EventArgs) Handles btnIgual.Click
+        EvaluaHaceOperacion()
+        operacion = ""
+    End Sub
+
+    Private Sub btn1_Click(sender As Object, e As EventArgs) Handles btn1.Click
+        EvaluaRestriccionConcatena()
+        txtDatos.Text &= "1"
+
+    End Sub
+
+    Private Sub btn2_Click(sender As Object, e As EventArgs) Handles btn2.Click
+        EvaluaRestriccionConcatena()
+        txtDatos.Text &= "2"
+    End Sub
+
+    Private Sub btn3_Click(sender As Object, e As EventArgs) Handles btn3.Click
+        EvaluaRestriccionConcatena()
+        txtDatos.Text &= "3"
+    End Sub
+
+    Private Sub btn4_Click(sender As Object, e As EventArgs) Handles btn4.Click
+        EvaluaRestriccionConcatena()
+        txtDatos.Text &= "4"
+    End Sub
+
+    Private Sub btn5_Click(sender As Object, e As EventArgs) Handles btn5.Click
+        EvaluaRestriccionConcatena()
+        txtDatos.Text &= "5"
+    End Sub
+
+    Private Sub btn6_Click(sender As Object, e As EventArgs) Handles btn6.Click
+        EvaluaRestriccionConcatena()
+        txtDatos.Text &= "6"
+    End Sub
+
+    Private Sub btn7_Click(sender As Object, e As EventArgs) Handles btn7.Click
+        EvaluaRestriccionConcatena()
+        txtDatos.Text &= "7"
+    End Sub
+
+    Private Sub btn8_Click(sender As Object, e As EventArgs) Handles btn8.Click
+        EvaluaRestriccionConcatena()
+        txtDatos.Text &= "8"
+    End Sub
+
+    Private Sub btn9_Click(sender As Object, e As EventArgs) Handles btn9.Click
+        EvaluaRestriccionConcatena()
+        txtDatos.Text &= "9"
+    End Sub
+
+    Private Sub btnCero_Click(sender As Object, e As EventArgs) Handles btnCero.Click
+        EvaluaRestriccionConcatena()
+        txtDatos.Text &= "0"
+    End Sub
+
+    Private Sub btnPunto_Click(sender As Object, e As EventArgs) Handles btnPunto.Click
+
+        EvaluaRestriccionConcatena()
+
+        If InStr(txtDatos.Text, ".", CompareMethod.Text) = 0 Then
+
+            txtDatos.Text &= "."
+        End If
+    End Sub
+
+    Public Sub EvaluaHaceOperacion()
+
+        sePresionaOperador = True
+        segundoValor = Val(txtDatos.Text)
+
+        If numeroResultado IsNot Nothing Then
+
+            Select Case operacion
+
+                Case "+"
+                    numeroResultado += segundoValor
+
+                Case "-"
+                    numeroResultado -= segundoValor
+
+                Case "*"
+                    numeroResultado *= segundoValor
+
+                Case "/"
+                    numeroResultado /= segundoValor
+
+            End Select
+            txtDatos.Text = numeroResultado
+
+        End If
+        numeroResultado = segundoValor
+
+    End Sub
+
+    Private Sub btnMas_Click(sender As Object, e As EventArgs) Handles btnMas.Click
+        EvaluaHaceOperacion()
+        operacion = "+"
+    End Sub
+
+    Private Sub btnMenos_Click(sender As Object, e As EventArgs) Handles btnMenos.Click
+        EvaluaHaceOperacion()
+        operacion = "-"
+    End Sub
+
+    Private Sub btnPor_Click(sender As Object, e As EventArgs) Handles btnPor.Click
+        EvaluaHaceOperacion()
+        operacion = "*"
+    End Sub
+
+    Private Sub btnEntre_Click(sender As Object, e As EventArgs) Handles btnEntre.Click
+        EvaluaHaceOperacion()
+        operacion = "/"
+    End Sub
+
+    Public Sub EvaluaRestriccionConcatena()
+        If sePresionaOperador = True Then
+            txtDatos.Text = ""
+            sePresionaOperador = False
+
+        ElseIf txtDatos.Text = "0" Then
+            txtDatos.Text = ""
+        End If
+
+    End Sub
+
+    Private Sub btnLimpiar_Click(sender As Object, e As EventArgs) Handles btnLimpiar.Click
+        txtDatos.Text = "0"
+    End Sub
+End Class
