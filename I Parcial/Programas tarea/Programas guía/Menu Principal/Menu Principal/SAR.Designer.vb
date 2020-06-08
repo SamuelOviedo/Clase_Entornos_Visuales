1fccf5b15c1c0e92ecedf2a3734c4cfc1231eb6c
Samuel Oviedo <isaaccoviedo@gmail.com>
2020-06-07 20:48:04 -0600

    SAR finalizado

--- I Parcial/Programas tarea/Programas guía/Menu Principal/Menu Principal/SAR.Designer.vb
+++ I Parcial/Programas tarea/Programas guía/Menu Principal/Menu Principal/SAR.Designer.vb
@@ -25,13 +25,28 @@ Partial Class SAR
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(SAR))
         Me.btnSalir = New System.Windows.Forms.Button()
         Me.btnHome = New System.Windows.Forms.Button()
+        Me.chkDeduccion = New System.Windows.Forms.CheckBox()
+        Me.chkExcento = New System.Windows.Forms.CheckBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtSueldoNeto = New System.Windows.Forms.TextBox()
+        Me.txtSueldo = New System.Windows.Forms.TextBox()
+        Me.btnCalcular = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtImpResultado = New System.Windows.Forms.TextBox()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
         Me.SuspendLayout()
         '
         'btnSalir
         '
         Me.btnSalir.BackColor = System.Drawing.Color.Firebrick
         Me.btnSalir.Font = New System.Drawing.Font("Shrikhand", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.btnSalir.Location = New System.Drawing.Point(634, 396)
+        Me.btnSalir.Location = New System.Drawing.Point(634, 451)
         Me.btnSalir.Name = "btnSalir"
         Me.btnSalir.Size = New System.Drawing.Size(154, 42)
         Me.btnSalir.TabIndex = 0
@@ -48,19 +63,184 @@ Partial Class SAR
         Me.btnHome.TabIndex = 1
         Me.btnHome.UseVisualStyleBackColor = True
         '
+        'chkDeduccion
+        '
+        Me.chkDeduccion.AutoSize = True
+        Me.chkDeduccion.Enabled = False
+        Me.chkDeduccion.FlatAppearance.BorderColor = System.Drawing.Color.Black
+        Me.chkDeduccion.FlatAppearance.BorderSize = 5
+        Me.chkDeduccion.FlatAppearance.CheckedBackColor = System.Drawing.Color.Transparent
+        Me.chkDeduccion.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent
+        Me.chkDeduccion.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent
+        Me.chkDeduccion.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.chkDeduccion.Font = New System.Drawing.Font("Rockwell", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.chkDeduccion.Location = New System.Drawing.Point(50, 51)
+        Me.chkDeduccion.Name = "chkDeduccion"
+        Me.chkDeduccion.Size = New System.Drawing.Size(134, 28)
+        Me.chkDeduccion.TabIndex = 2
+        Me.chkDeduccion.Text = "Deducción"
+        Me.chkDeduccion.UseVisualStyleBackColor = True
+        '
+        'chkExcento
+        '
+        Me.chkExcento.AutoSize = True
+        Me.chkExcento.Enabled = False
+        Me.chkExcento.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.chkExcento.Font = New System.Drawing.Font("Rockwell", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.chkExcento.Location = New System.Drawing.Point(50, 98)
+        Me.chkExcento.Name = "chkExcento"
+        Me.chkExcento.Size = New System.Drawing.Size(105, 28)
+        Me.chkExcento.TabIndex = 3
+        Me.chkExcento.Text = "Excento"
+        Me.chkExcento.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.BackColor = System.Drawing.Color.Silver
+        Me.GroupBox1.Controls.Add(Me.chkExcento)
+        Me.GroupBox1.Controls.Add(Me.chkDeduccion)
+        Me.GroupBox1.Font = New System.Drawing.Font("Shrikhand", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox1.Location = New System.Drawing.Point(443, 47)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(223, 182)
+        Me.GroupBox1.TabIndex = 4
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Aplica"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.BackColor = System.Drawing.Color.Silver
+        Me.GroupBox2.Controls.Add(Me.btnCalcular)
+        Me.GroupBox2.Controls.Add(Me.txtSueldo)
+        Me.GroupBox2.Font = New System.Drawing.Font("Shrikhand", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox2.Location = New System.Drawing.Point(24, 30)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(363, 216)
+        Me.GroupBox2.TabIndex = 5
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Ingresar sueldo"
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.BackColor = System.Drawing.Color.Silver
+        Me.GroupBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.GroupBox3.Controls.Add(Me.txtImpResultado)
+        Me.GroupBox3.Controls.Add(Me.Label2)
+        Me.GroupBox3.Controls.Add(Me.Button1)
+        Me.GroupBox3.Controls.Add(Me.txtSueldoNeto)
+        Me.GroupBox3.Controls.Add(Me.Label1)
+        Me.GroupBox3.Font = New System.Drawing.Font("Shrikhand", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox3.Location = New System.Drawing.Point(86, 262)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(627, 183)
+        Me.GroupBox3.TabIndex = 6
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "Resultados"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Russo One", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(281, 46)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(169, 27)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Sueldo neto:"
+        '
+        'txtSueldoNeto
+        '
+        Me.txtSueldoNeto.Font = New System.Drawing.Font("Rockwell", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtSueldoNeto.Location = New System.Drawing.Point(339, 76)
+        Me.txtSueldoNeto.Name = "txtSueldoNeto"
+        Me.txtSueldoNeto.Size = New System.Drawing.Size(271, 35)
+        Me.txtSueldoNeto.TabIndex = 1
+        '
+        'txtSueldo
+        '
+        Me.txtSueldo.Font = New System.Drawing.Font("Rockwell", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtSueldo.Location = New System.Drawing.Point(39, 64)
+        Me.txtSueldo.Name = "txtSueldo"
+        Me.txtSueldo.Size = New System.Drawing.Size(194, 35)
+        Me.txtSueldo.TabIndex = 0
+        '
+        'btnCalcular
+        '
+        Me.btnCalcular.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.btnCalcular.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.btnCalcular.Location = New System.Drawing.Point(131, 115)
+        Me.btnCalcular.Name = "btnCalcular"
+        Me.btnCalcular.Size = New System.Drawing.Size(210, 54)
+        Me.btnCalcular.TabIndex = 1
+        Me.btnCalcular.Text = "Calcular"
+        Me.btnCalcular.UseVisualStyleBackColor = False
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.SkyBlue
+        Me.Button1.Font = New System.Drawing.Font("Shrikhand", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(202, 129)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(230, 36)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Limpiar"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Russo One", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(39, 46)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(98, 27)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Sueldo:"
+        '
+        'txtImpResultado
+        '
+        Me.txtImpResultado.Font = New System.Drawing.Font("Rockwell", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtImpResultado.Location = New System.Drawing.Point(69, 76)
+        Me.txtImpResultado.Name = "txtImpResultado"
+        Me.txtImpResultado.Size = New System.Drawing.Size(186, 35)
+        Me.txtImpResultado.TabIndex = 4
+        '
         'SAR
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(800, 505)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
         Me.Controls.Add(Me.btnHome)
         Me.Controls.Add(Me.btnSalir)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.Name = "SAR"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "SAR"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
         Me.ResumeLayout(False)
 
     End Sub
 
     Friend WithEvents btnSalir As Button
     Friend WithEvents btnHome As Button
+    Friend WithEvents chkDeduccion As CheckBox
+    Friend WithEvents chkExcento As CheckBox
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents btnCalcular As Button
+    Friend WithEvents txtSueldo As TextBox
+    Friend WithEvents txtSueldoNeto As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents txtImpResultado As TextBox
+    Friend WithEvents Label2 As Label
 End Class
--- I Parcial/Programas tarea/Programas guía/Menu Principal/Menu Principal/SAR.vb
+++ I Parcial/Programas tarea/Programas guía/Menu Principal/Menu Principal/SAR.vb
@@ -9,4 +9,71 @@
         Me.Hide()
 
     End Sub
+
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles chkDeduccion.CheckedChanged
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnCalcular.Click
+
+        Dim sueldo As Double
+
+        sueldo = Val(txtSueldo.Text)
+
+
+        If txtSueldo.Text = "" Then
+            MsgBox("Escriba un valor en el sueldo", vbExclamation + vbOK, "Advertencia")
+
+        ElseIf Not IsNumeric(txtSueldo.Text) Then
+            MsgBox("Ingrese un valor numerico en el sueldo", vbExclamation + vbOK, "Advertencia")
+            txtSueldo.Clear()
+
+        ElseIf txtSueldo.Text < 0 Then
+            MsgBox("Ingrese un valor positivo en el sueldo", vbExclamation + vbOK, "Advertencia")
+            txtSueldo.Clear()
+        End If
+
+        Limpiar()
+
+        If sueldo < 16582.92 Then
+            chkExcento.Checked = True
+            txtSueldoNeto.Text = 16582.92
+
+        ElseIf (sueldo >= 16528.93 And sueldo <= 23536.61) Then
+            chkDeduccion.Checked = True
+            txtSueldoNeto.Text = sueldo - (sueldo * 0.15)
+
+        ElseIf (sueldo > 23536.62 And sueldo <= 50317.69) Then
+            chkDeduccion.Checked = True
+            txtSueldoNeto.Text = sueldo - (sueldo * 0.2)
+
+        ElseIf (sueldo > 50317.69) Then
+            chkDeduccion.Checked = True
+            txtSueldoNeto.Text = sueldo - (sueldo * 0.25)
+        End If
+
+        txtImpResultado.Text = sueldo
+    End Sub
+
+    Private Sub txtSueldoNeto_TextChanged(sender As Object, e As EventArgs) Handles txtSueldoNeto.TextChanged
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles Button1.Click
+        Limpiar()
+        txtSueldo.Clear()
+
+    End Sub
+
+    Private Sub Limpiar()
+        chkDeduccion.Checked = False
+        chkExcento.Checked = False
+        txtSueldoNeto.Clear()
+        txtImpResultado.Clear()
+
+    End Sub
 End Class
